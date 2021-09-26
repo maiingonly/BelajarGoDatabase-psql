3c5e5954931a70562c666c945351fdba3520078b
maiingonly <maherboleax@gmail.com>
2021-09-26 12:36:16 +0700

    query sql

--- sql_test.go
+++ sql_test.go
@@ -11,6 +11,7 @@ func TestExecSql(t *testing.T) {
 	defer db.Close()
 
 	ctx := context.Background()
+	//Exec context operation for no need return
 	// update := "UPDATE users SET id= 1, first_name='otong' WHERE email='tong@otong.com';"
 	delete := "DELETE FROM users WHERE id=2;"
 	// users := "INSERT INTO users (id, first_name, last_name, age, email) VALUES (2,'dinda', 'sudinda', 20, 'din@dinda.com');"
@@ -20,4 +21,19 @@ func TestExecSql(t *testing.T) {
 	}
 
 	fmt.Println("success insert new customer")
+}
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	script := "SELECT id, first_name FROM users"
+	ctx := context.Background()
+
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
 }
